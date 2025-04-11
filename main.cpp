4bdfd8e107bfd7f59f98d0ca6844f5e1baa02113
kudouyuki209 <k024g0023@g.neec.ac.jp>
2025-04-11 12:27:31 +0900

    s

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
-#include "../DirectXGame/math/Vector3.h"
-#include "Function.h"
 #include <Novice.h>
-const char kWindowTitle[] = "LD2A_01_クドウユウキ_タイトル";
 
-static const int kColumnWidth = 60;
-static const int kColumnHeight = 30;
+const char kWindowTitle[] = "学籍番号";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -28,16 +24,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		Vector3 v1{1.0f, 3.0f, -5.0f};
-		Vector3 v2{4.0f, -1.0f, 2.0f};
-		float k = {4.0f};
 
-		Vector3 resultAdd = Add(v1, v2);
-		Vector3 resultSubtract = Subtract(v1, v2);
-		Vector3 resultMultiply = Multiply(k, v1);
-		float resultDot = Dot(v1, v2);
-		float resultLength = Length(v1);
-		Vector3 resultNormalize = Normalize(v2);
 		///
 		/// ↑更新処理ここまで
 		///
@@ -45,13 +32,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
-		VectorScreenPrintf(0, 0, resultAdd, " : Add");
-		VectorScreenPrintf(0, kColumnHeight, resultSubtract, " : Subtract");
-		VectorScreenPrintf(0, 2 * kColumnHeight, resultMultiply, " : Multiply");
-		Novice::ScreenPrintf(0, 3 * kColumnHeight, "%0.2f : Dot", resultDot);
-		Novice::ScreenPrintf(0, 4 * kColumnHeight, "%0.2f : Length", resultLength);
-		VectorScreenPrintf(0, 5 * kColumnHeight, resultNormalize, " : Normalize");
 
 		///
 		/// ↑描画処理ここまで
