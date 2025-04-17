376f524613f767ba83eeb529e6936a22c1361d72
kudouyuki209 <k024g0023@g.neec.ac.jp>
2025-04-18 08:03:03 +0900

    00_03_確認課題

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ const char kWindowTitle[] = "LD2A_01_クドウユウキ_タイトル";
 static const int kColumnWidth = 60;
 static const int kRowHeight = 30;
 
-
-
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -35,27 +33,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Vector3 translate{4.1f, 2.6f, 0.8f};
 		Vector3 scale{1.5f, 5.2f, 7.3f};
 
-
 		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
 		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
 
-		Vector3 point{2.3f, 3.8f,1.4f};
+		Vector3 point{2.3f, 3.8f, 1.4f};
 
-		Matrix4x4 trasformMatrix = {
-		// 列0
-		 1.0f, 2.0f, 3.0f, 4.0f,
-        // 列1
-	     3.0f, 1.0f, 1.0f, 2.0f,
-	    // 列2
-	    1.0f, 4.0f, 2.0f, 3.0f,
-		// 列3
-		2.0f, 2.0f, 1.0f, 3.0f
-		
+		Matrix4x4 trasformMatrix = {// 列0
+		                            1.0f, 2.0f, 3.0f, 4.0f,
+		                            // 列1
+		                            3.0f, 1.0f, 1.0f, 2.0f,
+		                            // 列2
+		                            1.0f, 4.0f, 2.0f, 3.0f,
+		                            // 列3
+		                            2.0f, 2.0f, 1.0f, 3.0f
+
 		};
 
 		Vector3 transformed = Transform(point, trasformMatrix);
 
-		
 		///
 		/// ↑更新処理ここまで
 		///
@@ -65,10 +60,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		VectorScreenPrintf(0, 0, transformed, "transformed");
-		MatrixScreenPrintf(0, kRowHeight*1, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 1, translateMatrix, "translateMatrix");
 		MatrixScreenPrintf(0, kRowHeight * 5, scaleMatrix, "scaleMatrix");
-
-		
 
 		///
 		/// ↑描画処理ここまで
