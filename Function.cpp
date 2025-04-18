14381e19b32b93de379fa455f053ffaffae7830f
kudouyuki209 <k024g0023@g.neec.ac.jp>
2025-04-18 11:27:54 +0900

    01_01_確認課題

--- Function.cpp
+++ Function.cpp
@@ -9,94 +9,45 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label
 
 	for (int row = 0; row < 4; ++row) {
 		for (int colum = 0; colum < 4; ++colum) {
-			Novice::ScreenPrintf(x + colum * kColumnWidth, y + (row + 1) * kRowHeight, "%3.02f", matrix.m[row][colum]);
+			Novice::ScreenPrintf(x + colum * kColumnWidth, y + (row + 1) * kRowHeight, "%6.02f", matrix.m[row][colum]);
 		}
 	}
 
 	Novice::ScreenPrintf(x, y, "%s", label);
 }
 
-Matrix4x4 MakeTrancelateMatrix(Vector3 translate) {
+Matrix4x4 MakePrespectiveMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
 	Matrix4x4 result{};
-	result.m[0][0] = 1.0f;
-	result.m[1][1] = 1.0f;
-	result.m[2][2] = 1.0f;
-	result.m[3][0] = translate.x;
-	result.m[3][1] = translate.y;
-	result.m[3][2] = translate.z;
-	result.m[3][3] = 1.0f;
-
+	float f = 1.0f / std::tanf(fovY * 0.5f);
+	result.m[0][0] = f / aspectRatio;
+	result.m[1][1] = f;
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1.0f;
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+	result.m[3][3] = 0.0f;
 	return result;
 }
 
-Matrix4x4 MakeScaleMatrix(Vector3 scale) {
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
 	Matrix4x4 result{};
-	result.m[0][0] = scale.x;
-	result.m[1][1] = scale.y;
-	result.m[2][2] = scale.z;
-	result.m[3][3] = 1.0f;
-
-	return result;
-}
-
-Matrix4x4 MakeRotateXMatrix(float radian) {
-	Matrix4x4 result{};
-	result.m[0][0] = 1.0f;
-	result.m[1][1] = std::cosf(radian);
-	result.m[1][2] = std::sinf(radian);
-	result.m[2][1] = -std::sinf(radian);
-	result.m[2][2] = std::cosf(radian);
+	result.m[0][0] = 2.0f / (right - left);
+	result.m[1][1] = 2.0f / (top - bottom);       // ← DirectXならここが正しい
+	result.m[2][2] = 1.0f / (farClip - nearClip); // ← Z奥行き
+	result.m[3][0] = -(right + left) / (right - left);
+	result.m[3][1] = -(top + bottom) / (top - bottom);
+	result.m[3][2] = -nearClip / (farClip - nearClip);
 	result.m[3][3] = 1.0f;
 	return result;
 }
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) { 
 
-Matrix4x4 MakeRotateYMatrix(float radian) {
 	Matrix4x4 result{};
-	result.m[0][0] = std::cosf(radian);
-	result.m[0][2] = -std::sinf(radian);
-	result.m[1][1] = 1.0f;
-	result.m[2][0] = std::sinf(radian);
-	result.m[2][2] = std::cosf(radian);
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -height / 2.0f;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][0] = left + width / 2.0f;
+	result.m[3][1] = top + height / 2.0f;
+	result.m[3][2] = minDepth;
 	result.m[3][3] = 1.0f;
 	return result;
 }
-
-Matrix4x4 MakeRotateZMatrix(float radian) {
-	Matrix4x4 result{};
-	result.m[0][0] = std::cosf(radian);
-	result.m[0][1] = std::sinf(radian);
-	result.m[1][0] = -std::sinf(radian);
-	result.m[1][1] = std::cosf(radian);
-	result.m[2][2] = 1.0f;
-	result.m[3][3] = 1.0f;
-	return result;
-}
-
-
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
-	Matrix4x4 result{};
-	for (int row = 0; row < 4; ++row) {
-		for (int colum = 0; colum < 4; ++colum) {
-			result.m[row][colum] = m1.m[row][0] * m2.m[0][colum] + m1.m[row][1] * m2.m[1][colum] + m1.m[row][2] * m2.m[2][colum] + m1.m[row][3] * m2.m[3][colum];
-		}
-	}
-	return result;
-}
-
-
-Matrix4x4 MakeAffineMatrix(Vector3 scale, Vector3 rotate, Vector3 translate) {
-	Matrix4x4 result{};
-
-	Matrix4x4 rotateX = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateY = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZ = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateXYZ = Multiply(rotateX, Multiply(rotateY, rotateZ));
-
-	Matrix4x4 ScaleRotateMatrix = Multiply(MakeScaleMatrix(scale), rotateXYZ);
-	result = Multiply(ScaleRotateMatrix, MakeTrancelateMatrix(translate));
-
-
-	return result;
-
-}
-
--- Function.h
+++ Function.h
@@ -6,3 +6,9 @@
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
 
 Matrix4x4 MakeAffineMatrix(Vector3 scale, Vector3 rotate, Vector3 translate);
+
+Matrix4x4 MakePrespectiveMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
